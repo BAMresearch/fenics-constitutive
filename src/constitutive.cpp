e43f1a30ec27f684379866189cb59c3dcfd8c949
Thomas Titscher <thomas.titscher@bam.de>
2020-12-02 15:23:01 +0100

    Template pybind sucks

--- src/constitutive.cpp
+++ src/constitutive.cpp
@@ -4,9 +4,20 @@
 #include <strain.h>
 
 namespace py = pybind11;
+using namespace constitutive;
 
 PYBIND11_MODULE(cpp, m)
 {
     m.doc() = "constitutive";
-    m.def("times_two", &constitutive::timesTwo, "doc");
+    m.def("times_two", &timesTwo, "doc");
+
+    using Mandel2 = Mandel<2>;
+    using MandelX2 = MandelX<2>;
+
+    py::class_<Mandel2, Eigen::Vector3d> mandel2(m, "Mandel2");
+    mandel2.def(py::init<Eigen::Vector3d>());
+
+    py::class_<MandelX2, Eigen::Vector4d> mandelX2(m, "MandelX2");
+    mandelX2.def(py::init<Eigen::Vector4d>());
+    mandelX2.def(py::init<Mandel2>());
 }
--- src/strain.h
+++ src/strain.h
@@ -1,9 +1,73 @@
 #pragma once
 
-#include <Eigen/Core>
+#include <eigen3/Eigen/Core>
 
 namespace constitutive
 {
+
+struct Dim
+{
+    static constexpr int Mandel(int d)
+    {
+        return d == 1 ? 1 : (d == 2 ? 3 : 6);
+    }
+
+    static constexpr int MandelX(int d)
+    {
+        return d == 1 ? 3 : (d == 2 ? 4 : 6);
+    }
+};
+
+
+template <int TDim>
+class Mandel : public Eigen::Matrix<double, Dim::Mandel(TDim), 1>
+{
+    using Parent = Eigen::Matrix<double, Dim::Mandel(TDim), 1>;
+
+public:
+    Mandel(const Parent& other)
+        : Parent(other)
+    {
+    }
+};
+
+template <int TDim>
+class MandelX : public Eigen::Matrix<double, Dim::MandelX(TDim), 1>
+{
+    using Parent = Eigen::Matrix<double, Dim::MandelX(TDim), 1>;
+
+public:
+    MandelX(const Parent& other)
+        : Parent(other)
+    {
+    }
+
+    MandelX(const Mandel<TDim>& other);
+};
+
+template <>
+inline MandelX<1>::MandelX(const Mandel<1>& other)
+{
+    (*this)[0] = other[0];
+    (*this)[1] = 0;
+    (*this)[2] = 0;
+}
+
+template <>
+inline MandelX<2>::MandelX(const Mandel<2>& other)
+{
+    (*this)[0] = other[0];
+    (*this)[1] = other[1];
+    (*this)[2] = 0;
+    (*this)[3] = other[2];
+}
+
+template <>
+inline MandelX<3>::MandelX(const Mandel<3>& other)
+{
+    (*this) = other;
+}
+
 Eigen::Vector3d timesTwo(Eigen::Vector3d a)
 {
     return a * 2.;
