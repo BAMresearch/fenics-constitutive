d5303846b7f698e963d1d8a415e7c7e1cd936ca3
srosenbu <sjard-mathis.rosenbusch@bam.de>
2021-08-04 15:19:47 +0200

    Add von mises yield function

--- src/plasticity.h
+++ src/plasticity.h
@@ -17,28 +17,28 @@ class IsotropicHardeningPlasticity : public LawInterface
 {
 public:
     std::shared_ptr<YieldFunction> _f;
-    std::shared_ptr<IsotropicHardeningLaw> _h;
+    std::shared_ptr<IsotropicHardeningLaw> _p;
     std::shared_ptr<FlowRule> _g;
-    std::vector<QValues> _kappa_0;
-    std::vector<QValues> _kappa_1;
+    std::vector<QValues> _internal_vars_0;
+    std::vector<QValues> _internal_vars_1;
     Eigen::MatrixXd _C;
     bool _total_strains;
     bool _tangent;
     
-    Plasticity(EigenMatrixXd& C, std::shared_ptr<YieldFunction> f, std::shared_ptr<FlowRule> g, std::shared_ptr<IsotropicHardeningLaw> h, bool total_strains = true, bool tangent = true)
+    Plasticity(EigenMatrixXd& C, std::shared_ptr<YieldFunction> f, std::shared_ptr<FlowRule> g, std::shared_ptr<IsotropicHardeningLaw> p, bool total_strains = true, bool tangent = true)
     : _f(f),
-    _h(h),
+    _p(p),
     _g(g),
     _total_strains(total_strains),
     _tangent(tangent),
     {
 
-        _kappa_0[LAMBDA] = QValues(1);
-        _kappa_1[LAMBDA] = QValues(1);
+        _internal_vars_0[KAPPA] = QValues(1);
+        _internal_vars_1[KAPPA] = QValues(1);
         if (_total_strains)
         {
-            _kappa_0[EPS_P] = QValues(6);
-            _kappa_1[EPS_P] = QValues(6);
+            _internal_vars_0[EPS_P] = QValues(6);
+            _internal_vars_1[EPS_P] = QValues(6);
         }
     }
     
@@ -60,9 +60,9 @@ public:
     void Evaluate(const std::vector<QValues>& input, std::vector<QValues>& out, int i) override
     {
         auto strain = input[EPS].Get(i);
-        auto plastic_strain = _kappa_0[EPS_P].Get(i);
+        auto plastic_strain = _internal_vars_0[EPS_P].Get(i);
         if (_total_strains) {
-            auto sigma_tr = _C * (strain-plastic_strain);
+            auto sigma_tr = _C * (strain - plastic_strain);
         } else {
             auto sigma_tr = input[SIGMA].Get(i) + _C * strain;
         }
@@ -95,8 +95,8 @@ struct NewtonFunction
 
 struct YieldFunction
 {
-    virtual void Evaluate(Eigen::VectorXd& sigma, Eigen::VectorXd& kappa);
-    virtual void Evaluate(Eigen::VectorXd& sigma);
+    virtual void Evaluate(Eigen::VectorXd sigma, Eigen::VectorXd kappa);
+    virtual void Evaluate(Eigen::VectorXd sigma);
     
     //Get will return the i-th derivative 
     //of the yield function with respect to 
@@ -108,37 +108,53 @@ struct YieldFunction
 
 class MisesYieldFunction
 {
-
+public:
     int _n;
-    MisesYieldFunction(n_derivatives = 3)
-    : _n(n_derivatives)
+    double _sig_0;
+    double _H;
+    double _f;
+    Eigen::VectorXd _df_dsig;
+    Eigen::MatrixXd _ddf_dsig;
+
+    MisesYieldFunction(n_derivatives = 3, double sig_0, double H)
+    : _n(n_derivatives),
+      _sig_0(sig_0),
+      _H(H)
     {
     }
-    virtual void Evaluate(Eigen::VectorXd& sigma, Eigen::VectorXd& kappa)
+
+    virtual void Evaluate(Eigen::VectorXd sigma, Eigen::VectorXd kappa) override
     {
-        auto sigma_dev = T_dev * sigma;
-        auto f = sigma_dev.dot(sigma_dev);
+        auto sig_dev = T_dev * sigma;
+        auto sig_eq = std::sqrt(1.5 * sigma_dev.dot(sigma_dev));
+        _f = sig_eq - _sig_0 - _H*kappa;
+        _df_dsig = (1.5 / sig_eq) * sig_dev; //actually a row vector. Use .transpose()
+        _ddf_dsig =  1.5 * (T_dev /sig_eq - sig_dev * _df_dsig.transpose()/ (sig_eq*sig_eq));
     }
-    virtual void Evaluate(Eigen::VectorXd& sigma);
+
+    virtual void Evaluate(Eigen::VectorXd sigma);
     
     //Get will return the i-th derivative 
     //of the yield function with respect to 
     //the stress. i=0 means the pure function
     //evaluation. In associative plasticity
     //we need up to i=2.
-    Eigen::MatrixXd Get(int i) = 0;
+    //std::tuple<double, Eigen::VectorXd, Eigen::MatrixXd> Get() override
+    //{
+    //    return {_f, _df_dsig, _ddf_dsig};
+    //}
 };
+
 struct FlowRule
 {
     bool _associative;
-    virtual void Evaluate(Eigen::VectorXd& sigma, Eigen::VectorXd& kappa);
-    virtual void Evaluate(Eigen::VectorXd& sigma);
+    virtual void Evaluate(Eigen::VectorXd sigma, Eigen::VectorXd kappa);
+    virtual void Evaluate(Eigen::VectorXd sigma);
     
     //Get will return the i-th derivative
     //of the flow rule with respect to 
     //the stress. i=0 means the pure function
-    //evaluation. In associative plasticity
-    //we need up to i=2.
+    //evaluation. 
     Eigen::MatrixXd Get(int i) = 0;
 };
 
@@ -155,6 +171,5 @@ public:
 
 struct IsotropicHardeningLaw
 {
-    double Evaluate(Eigen::VectorXd kappa) = 0;
+    double Evaluate(Eigen::VectorXd sigma, double kappa) = 0;
 };
-
