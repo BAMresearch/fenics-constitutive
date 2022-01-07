e46b2c07d59902d10cd3ee92e11a3651bb705579
etamsen <erik.tamsen@bam.de>
2022-01-07 14:53:01 +0100

    initial idea for parameterstudy

--- module/truss_convergence.py
+++ module/truss_convergence.py
@@ -10,6 +10,7 @@ analytical solution
 """
 import dolfin as df
 import numpy as np
+import matplotlib.pyplot as plt
 
 # MODULE "SENSOR"
 
@@ -48,6 +49,9 @@ class UniaxialTrussExperiment(Experiment):
             return x[0] < df.DOLFIN_EPS and on_boundary
 
         return [df.DirichletBC(V, df.Constant(0.0), left)]
+
+    def update_param(self, parameters):
+        self.mesh = df.IntervalMesh(1, 0.0, parameters["L"])
 
     def refine(self, N=1):
         """
@@ -162,7 +166,95 @@ def estimate_E(experiment, parameters, sensor):
     return optimize_result.x
 
 
+# Question: would it be better to "feed" a problem instead of experiment + parameters?
+def run_paramterstudy(experiment, parameters, sensor, param_list = None, scale_list = [0.25, 0.5, 2.0, 3.0]):
+    # TODO: 1) how to decide what kind of output is required? 2D/3D plot, points/lines?
+    #          maybe somehow define this in the sensor?
+    #       2) return only the list and do the plotting "postprocessing" outside the function, add the plot as a flag?
+    #       3) what to do when adding time dependency? timestep freuquency as parameters? point -> line, line -> area (3D/colorplot?)
+    #       4) currently problem if paramter is not float (eg. dimension). is this a relevant probem?
+    #       5) parameters are defined at multiple "levels/places", current workaround experiment.update
+    #          does not seem to be the best solution? also 2 calcs crash...
+    #          maybe a problem class that gets experiement, material and parameters? instead of passing param to both?
+
+    # Problem: parameters are somewho defined at multiple places..., how to change geometry paramters...
+
+    # setup problem
+    problem = LinearElasticity(experiment, parameters)
+
+    # generate list with paramters to analyze
+    if not param_list:
+        param_list = list(parameters.keys())
+    else:
+        # check if given name matches parameter, otherwise remove from list
+        delete = []
+        for item in param_list:
+            if not item in parameters:
+                print(f'AAAAHHHHHH {item} is not a valid parameter name!!!')
+                delete.append(item)
+        for item in delete:
+            param_list.remove(item)
+
+    # make a copy of the original parameter dictionary
+    og_params = parameters.copy()
+
+    # compute "standard" solution, to compare for all variations
+    u_fem = problem(sensor)
+
+    # createlist for results
+    plot_results = []
+    for key in param_list:
+        # add original calculation to all list
+        plot_results.append([[parameters[key],u_fem]])
+
+    # setup fiure
+    fig, axs = plt.subplots(len(param_list))
+    fig.suptitle('Paramterstudy of various paramters')
+    fig.tight_layout()
+
+    # loop over paramters and scale factors
+    for i, parameter in enumerate(param_list):
+        print(f'Computing parameter: {parameter}')
+
+        for scale in scale_list:
+            # adjust paramter in problem
+            problem.params[parameter] = og_params[parameter] * scale
+
+            experiment.update_param(problem.params)
+            try:
+                # maybe problem crashes because of strange parameter or other problem
+                u_fem = problem(sensor)
+                plot_results[i].append([problem.params[parameter],u_fem])
+            except:
+                # better error message and print which value caused the crash
+                print(f'Computation crashed for {parameter} = {problem.params[parameter]}')
+
+        # sort plot list
+        plot_results[i].sort()
+        # reorder data points, such that a x and a y list is given to plot
+        axs[i].plot(*zip(*plot_results[i]))
+        # set plot title
+        axs[i].set_title(parameter)
+
+        # reset the parameter to original value
+        problem.params[parameter] = og_params[parameter]
+
+    # show plot
+    plt.show()
+    return plot_results
+
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
+    # define experimental paramters
+    # this incudes the strucutral as well as the material paramters
     parameters = {
         "L": 42.0,
         "E": 10.0,
@@ -171,6 +263,7 @@ if __name__ == "__main__":
         "rho": 7.0,
         "degree": 1,
     }
+    # experiement class
     experiment = get_experiment("UniaxialTruss", parameters)
 
     # attach analytic solution for the full displacement field
@@ -184,23 +277,6 @@ if __name__ == "__main__":
     u_max = 0.5 * p["rho"] * p["g"] * p["A"] * p["L"] ** 2 / p["E"]
     experiment.add_sensor_data(u_sensor, u_max)
 
-    # First assume that we don't know E...
-    parameters["E"] = 1.0
-    # ...so we infer it using the measuremnts.
-    parameters["E"] = estimate_E(experiment, parameters, u_sensor)
 
-    # Run the convergence analysis with the maximum displacement. As for all
-    # nodal values, we expect them to be correct even for a single linear
-    # element.
-    for degree in [1, 2, 3]:
-        parameters["degree"] = degree
-        n_refinements = run_convergence(experiment, parameters, u_sensor)
-        assert n_refinements == 0
-
-    # Run the convergence analysis with the whole displacement field.
-    # Here, a linear solution can only be exact up to a given epsilon.
-    # Quadratic and cubic interpolation caputure the field without refinement.
-    for degree, expected_n_refinements in [(3, 0), (2, 0), (1, 14)]:
-        parameters["degree"] = degree
-        n_refinements = run_convergence(experiment, parameters, full_u_sensor)
-        assert n_refinements == expected_n_refinements
+    # Run the paramterstudy
+    run_paramterstudy(experiment, parameters, u_sensor, param_list=['E','g','L','degree'])
