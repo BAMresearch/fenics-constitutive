194dbd562f5f1d4be10f814fefaf8c3bc9e76928
srosenbu <sjard-mathis.rosenbusch@bam.de>
2022-03-30 11:58:30 +0200

    Import only cpp classes when dolfinx is installed

--- constitutive/__init__.py
+++ constitutive/__init__.py
@@ -1,20 +1,26 @@
+
 import warnings
-
-import dolfin as df
-from ffc.quadrature.deprecation import \
-   QuadratureRepresentationDeprecationWarning
-
-from .explicit_dynamics import *
-from .helper import *
-from .mechanics_problem import *
-from .cpp import *
-warnings.simplefilter("ignore", QuadratureRepresentationDeprecationWarning)
-df.parameters["form_compiler"]["representation"] = "quadrature"
-
 try:
-   from fenics_helpers import boundary as bc
-   from fenics_helpers.timestepping import TimeStepper
-except Exception as e:
-   print("Install fenics_helpers via (e.g.)")
-   print("   pip3 install git+https://github.com/BAMResearch/fenics_helpers")
-   raise (e)
+    import dolfinx as dfx
+    from .cpp import *
+except ModuleNotFoundError:
+    import dolfin as df
+
+
+    from ffc.quadrature.deprecation import \
+        QuadratureRepresentationDeprecationWarning
+
+    warnings.simplefilter("ignore", QuadratureRepresentationDeprecationWarning)
+    df.parameters["form_compiler"]["representation"] = "quadrature"
+    from .explicit_dynamics import *
+    from .helper import *
+    from .mechanics_problem import *
+    from .cpp import *
+
+    try:
+       from fenics_helpers import boundary as bc
+       from fenics_helpers.timestepping import TimeStepper
+    except Exception as e:
+       print("Install fenics_helpers via (e.g.)")
+       print("   pip3 install git+https://github.com/BAMResearch/fenics_helpers")
+       raise (e)
