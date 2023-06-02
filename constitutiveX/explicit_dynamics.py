af24ac2417014dcb0b864f564c8ac4fdb0efdc21
srosenbu <sjard-mathis.rosenbusch@bam.de>
2023-06-02 16:31:47 +0200

    3D class

--- constitutiveX/explicit_dynamics.py
+++ constitutiveX/explicit_dynamics.py
@@ -4,12 +4,171 @@ import numpy as np
 import ufl
 from petsc4py import PETSc
 import basix
-#import constitutive as c
+
+# import constitutive as c
 import constitutiveX.cpp as _cpp
 import dolfinx as dfx
+import ufl
+from constitutiveX.helpers import (
+    QuadratureRule,
+    QuadratureEvaluator,
+    get_local,
+    set_local,
+    add_local,
+    set_mesh_coordinates,
+)
 
-from constitutiveX.helpers import QuadratureRule, QuadratureEvaluator, get_local, set_local, add_local, set_mesh_coordinates
 
+class CDMX3D:
+    def __init__(
+        self,
+        function_space,
+        t0,
+        f_ext,
+        bcs,
+        M,
+        law,
+        quadrature_rule,
+        nonlocal_var=None,
+        damping=None,
+        b_bar=False,
+    ):
+        self.mesh = function_space.mesh
+        self.quadrature_rule = quadrature_rule
+        self.del_t = None
+        self.v = dfx.fem.Function(function_space, name="Velocity")
+        self.u = dfx.fem.Function(function_space, name="Displacements")
+
+        self.f = self.v.vector.copy()
+        self.damping = damping
+
+        self.QV = self.quadrature_rule.create_quadrature_vector_space(self.mesh, 6)
+
+        self.stress = dfx.fem.Function(self.QV, name="Mandel_Stress")
+
+        self.local_q_dim = self.quadrature_rule.number_of_points(self.mesh)
+
+        self.L = np.zeros(self.local_q_dim * 9)
+        self.t = t0
+        self.law = law
+
+        self.f_ext = f_ext
+
+        test_function = ufl.TestFunction(function_space)
+
+        self.f_int_ufl = (
+            -ufl.inner(
+                self._as_mandel(ufl.sym(ufl.grad(test_function))),
+                self.stress,
+            )
+            * self.quadrature_rule.dx
+        )
+
+        self.f_int_form = dfx.fem.form(self.f_int_ufl)
+
+        self.L_evaluator = QuadratureEvaluator(
+            ufl.nabla_grad(self.v),
+            self.mesh,
+            self.quadrature_rule,
+        )
+        self.bcs = bcs
+        self.M = M
+
+        self.nonlocal_var = nonlocal_var
+        self.b_bar = b_bar
+
+    def _as_mandel(self, T: ufl.core.expr.Expr):
+        """
+        T:
+            Symmetric 3x3 tensor
+        Returns:
+            Vector representation of T with factor sqrt(2) for off diagonal components
+        """
+        factor = 2**0.5
+        return ufl.as_vector(
+            [
+                T[0, 0],
+                T[1, 1],
+                T[2, 2],
+                factor * T[1, 2],
+                factor * T[0, 2],
+                factor * T[0, 1],
+            ]
+        )
+
+    # @profile
+    def stress_update(self, h):
+        self.L_evaluator(self.L)
+
+        _cpp.jaumann_rotate_fast_3d(self.L, self.stress.vector.array, h)
+        if self.b_bar:
+            _cpp.apply_b_bar_3d(self.L, self.quadrature_rule.weights.size)
+
+        input_list = [np.array([])] * _cpp.Q.LAST
+        input_list[_cpp.Q.GRAD_V] = self.L
+        input_list[_cpp.Q.SIGMA] = self.stress.vector.array
+
+        if self.nonlocal_var is not None:
+            input_list[
+                self.nonlocal_var.Q_nonlocal
+            ] = self.nonlocal_var.get_quadrature_values()
+
+        # TODO: Is GhostUpdate really used correcxtly
+        self.law.evaluate(input_list, h)
+        # TODO
+        self.stress.x.scatter_forward()
+
+    # @profile
+    def step(self, h):
+        del_t_mid = (h + self.del_t) / 2.0 if self.del_t is not None else h
+        self.del_t = h
+
+        with self.f.localForm() as f_local:
+            f_local.set(0.0)
+        # dfx.fem.petsc.assemble_vector(self.f, dfx.fem.form(self.f_int_ufl))
+        dfx.fem.petsc.assemble_vector(self.f, self.f_int_form)
+        # TODO
+        self.f.ghostUpdate(
+            addv=PETSc.InsertMode.ADD_VALUES, mode=PETSc.ScatterMode.REVERSE
+        )
+        # self.f.array[:]*=-1
+        if self.f_ext is not None:
+            self.f.array[:] += self.f_ext(self.t).array
+            # TODO
+            self.f.ghostUpdate(
+                addv=PETSc.InsertMode.INSERT_VALUES, mode=PETSc.ScatterMode.FORWARD
+            )
+            # self.f.__iadd__(self.f_ext(self.t))
+
+        # given: v_n-1/2, x_n/u_n, a_n, f_int_n
+        # Advance velocities and nodal positions in time
+        if self.damping is None:
+            c1 = 1.0
+            c2 = del_t_mid
+        else:
+            c1 = (2.0 - self.damping * del_t_mid) / (2.0 + self.damping * del_t_mid)
+            c2 = 2.0 * del_t_mid / (2.0 + self.damping * del_t_mid)
+
+        set_local(self.v, c1 * get_local(self.v) + c2 * self.M.array * self.f.array)
+
+        dfx.fem.set_bc(self.v.vector.array, self.bcs)
+        # ghost entries are needed
+        self.v.x.scatter_forward()
+        # use v.x instead of v.vector, since mesh update requires ghost entries
+        du_half = (0.5 * self.del_t) * self.v.vector.array
+
+        set_mesh_coordinates(self.mesh, du_half, mode="add")
+        if self.nonlocal_var is not None:
+            self.nonlocal_var.step(
+                self.del_t, self.law.get_internal_var(self.nonlocal_var.Q_local)
+            )
+        self.stress_update(self.del_t)
+
+        self.u.x.array[:] += 2.0 * du_half
+
+        set_mesh_coordinates(self.mesh, du_half, mode="add")
+
+        self.t += self.del_t
 
 
 class CDMPlaneStrainX:
@@ -68,7 +227,7 @@ class CDMPlaneStrainX:
         self.M = M
 
         self.nonlocal_var = nonlocal_var
-        self.b_bar=b_bar
+        self.b_bar = b_bar
 
     def __2d_tensor_to_3d(self, T):
         return ufl.as_matrix(
@@ -77,18 +236,26 @@ class CDMPlaneStrainX:
 
     def __2d_tensor_as_mandel(self, T):
         """
-        T: 
+        T:
             Symmetric 2x2 tensor
         Returns:
             Vector representation of T with factor sqrt(2) for off diagonal components
         """
-        factor = 2 ** 0.5
-        return ufl.as_vector([T[0, 0], T[1, 1], 0.0, 0.0, 0.0, factor * T[0, 1],])
+        factor = 2**0.5
+        return ufl.as_vector(
+            [
+                T[0, 0],
+                T[1, 1],
+                0.0,
+                0.0,
+                0.0,
+                factor * T[0, 1],
+            ]
+        )
 
     # @profile
     def stress_update(self, h):
         self.L_evaluator(self.L)
-
 
         _cpp.jaumann_rotate_fast_3d(self.L, self.stress.vector.array, h)
         if self.b_bar:
@@ -97,31 +264,37 @@ class CDMPlaneStrainX:
         input_list = [np.array([])] * _cpp.Q.LAST
         input_list[_cpp.Q.GRAD_V] = self.L
         input_list[_cpp.Q.SIGMA] = self.stress.vector.array
-        
-        if self.nonlocal_var is not None:
-           input_list[self.nonlocal_var.Q_nonlocal] = self.nonlocal_var.get_quadrature_values()
 
-        #TODO: Is GhostUpdate really used correcxtly
+        if self.nonlocal_var is not None:
+            input_list[
+                self.nonlocal_var.Q_nonlocal
+            ] = self.nonlocal_var.get_quadrature_values()
+
+        # TODO: Is GhostUpdate really used correcxtly
         self.law.evaluate(input_list, h)
-        #TODO
+        # TODO
         self.stress.x.scatter_forward()
 
     # @profile
     def step(self, h):
-        del_t_mid = (h + self.del_t)/2. if self.del_t is not None else h
+        del_t_mid = (h + self.del_t) / 2.0 if self.del_t is not None else h
         self.del_t = h
-        
+
         with self.f.localForm() as f_local:
             f_local.set(0.0)
         # dfx.fem.petsc.assemble_vector(self.f, dfx.fem.form(self.f_int_ufl))
         dfx.fem.petsc.assemble_vector(self.f, self.f_int_form)
-        #TODO
-        self.f.ghostUpdate(addv=PETSc.InsertMode.ADD_VALUES, mode=PETSc.ScatterMode.REVERSE)
+        # TODO
+        self.f.ghostUpdate(
+            addv=PETSc.InsertMode.ADD_VALUES, mode=PETSc.ScatterMode.REVERSE
+        )
         # self.f.array[:]*=-1
         if self.f_ext is not None:
             self.f.array[:] += self.f_ext(self.t).array
-            #TODO
-            self.f.ghostUpdate(addv=PETSc.InsertMode.INSERT_VALUES, mode=PETSc.ScatterMode.FORWARD)
+            # TODO
+            self.f.ghostUpdate(
+                addv=PETSc.InsertMode.INSERT_VALUES, mode=PETSc.ScatterMode.FORWARD
+            )
             # self.f.__iadd__(self.f_ext(self.t))
 
         # given: v_n-1/2, x_n/u_n, a_n, f_int_n
@@ -154,8 +327,9 @@ class CDMPlaneStrainX:
 
         self.t += self.del_t
 
+
 class NonlocalInterface:
-    def __init__(self, Q_local:_cpp.Q, Q_nonlocal:_cpp.Q):
+    def __init__(self, Q_local: _cpp.Q, Q_nonlocal: _cpp.Q):
         self.Q_local = Q_local
         self.Q_nonlocal = Q_nonlocal
 
@@ -168,8 +342,10 @@ class NonlocalInterface:
     def get_nodal_values(self):
         raise NotImplementedError("get_nodal_values needs to be implemented.")
 
+
 class ImplicitNonlocalVariable(NonlocalInterface):
     """This class should work with all constraints"""
+
     def __init__(
         self,
         Q_local,
@@ -195,20 +371,21 @@ class ImplicitNonlocalVariable(NonlocalInterface):
         test_function = ufl.TestFunction(function_space)
         trial_function = ufl.TrialFunction(function_space)
         b_form = ufl.inner(test_function, self.p_l) * self.quadrature_rule.dx
-        A_form = (ufl.inner(test_function,trial_function) + self.l**2. * ufl.inner(ufl.grad(test_function),ufl.grad(trial_function))) * self.quadrature_rule.dx
-    
+        A_form = (
+            ufl.inner(test_function, trial_function)
+            + self.l**2.0
+            * ufl.inner(ufl.grad(test_function), ufl.grad(trial_function))
+        ) * self.quadrature_rule.dx
 
-        self.problem = dfx.fem.petsc.LinearProblem(A_form, b_form,u=self.p_nl)
-        #p_nl_h = problem.solve()
-        
-        
+        self.problem = dfx.fem.petsc.LinearProblem(A_form, b_form, u=self.p_nl)
+        # p_nl_h = problem.solve()
+
         self.p_evaluator = QuadratureEvaluator(
             self.p_nl, self.mesh, self.quadrature_rule
         )
 
-
     def step(self, h, p, substeps=1):
-        #we assume that the local plastic strain is constant on all substeps
+        # we assume that the local plastic strain is constant on all substeps
         set_local(self.p_l, p)
         self.problem.solve()
 
