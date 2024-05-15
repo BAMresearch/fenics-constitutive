a33c8344f8f4c1da4e30db75fc3ae9e7b8fee15c
aradermacher <annika.robens-radermacher@bam.de>
2024-05-15 15:03:16 +0200

    work on 3D generalization kelvin model

--- examples/visco_elasticity/spring_kelvin_model.py
+++ examples/visco_elasticity/spring_kelvin_model.py
@@ -20,29 +20,71 @@ class SpringKelvinModel(IncrSmallStrainModel):
         self._constraint = constraint
         self.E0 = parameters["E0"] # elastic modulus
         self.E1 = parameters["E1"] # visco modulus
-        self.tau = parameters["tau"] # relaxation time == eta/E1
-        self.nu = parameters["nu"] # Poisson's ratio
+        self.tau = parameters["tau"] # relaxation time == eta/(2 mu1) for 1D case eta/E1
+        if Constraint.UNIAXIAL_STRESS:
+            self.nu = 0.0
+        else:
+            self.nu = parameters["nu"] # Poisson's ratio
+
         # lame constants (need to be updated if time dependent material parameters are used)
         self.mu0 = self.E0 / (2.0 * (1.0 + self.nu))
         self.lam0 = self.E0 * self.nu / ((1.0 + self.nu) * (1.0 - 2.0 * self.nu))
         self.mu1 = self.E1 / (2.0 * (1.0 + self.nu))
 
+        self.I2 = np.zeros(self.stress_strain_dim, dtype=np.float64)  # Identity of rank 2 tensor
+        self.compute_elasticity() # initialize elasticity tensor
 
-        # match constraint:
-        #     case Constraint.FULL:
-        #         self.D = None
-        #
-        #     case Constraint.PLANE_STRAIN:
-        #         self.D = None
-        #
-        #     case Constraint.PLANE_STRESS:
-        #         self.D = None
-        #
-        #     case Constraint.UNIAXIAL_STRESS:
-        #         self.D_E0 = np.array([[self.E0]])
-        #     case _:
-        #         msg = "Constraint not implemented"
-        #         raise NotImplementedError(msg)
+    def compute_elasticity(self):
+        match self._constraint:
+            case Constraint.FULL:
+                self.D_0 = np.array(
+                    [
+                        [2.0 * self.mu0 + self.lam0, self.lam0, self.lam0, 0.0, 0.0, 0.0],
+                        [self.lam0, 2.0 * self.mu0 + self.lam0, self.lam0, 0.0, 0.0, 0.0],
+                        [self.lam0, self.lam0, 2.0 * self.mu0 + self.lam0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 2.0 * self.mu0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 2.0 * self.mu0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 2.0 * self.mu0],
+                    ]
+                )
+                self.I2[0] = 1.0
+                self.I2[1] = 1.0
+                self.I2[2] = 1.0
+
+            case Constraint.PLANE_STRAIN:
+                self.D_0 = np.array(
+                    [
+                        [2.0 * self.mu0 + self.lam0, self.lam0, self.lam0, 0.0],
+                        [self.lam0, 2.0 * self.mu0 + self.lam0, self.lam0, 0.0],
+                        [self.lam0, self.lam0, 2.0 * self.mu0 + self.lam0, 0.0],
+                        [0.0, 0.0, 0.0, 2.0 * self.mu0],
+                    ]
+                )
+                self.I2[0] = 1.0
+                self.I2[1] = 1.0
+
+            case Constraint.PLANE_STRESS:
+                self.D_0 = (
+                        self.E0
+                        / (1 - self.nu ** 2.0)
+                        * np.array(
+                    [
+                        [1.0, self.nu, 0.0, 0.0],
+                        [self.nu, 1.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, (1.0 - self.nu)],
+                    ]
+                )
+                )
+                self.I2[0] = 1.0
+                self.I2[1] = 1.0
+
+            case Constraint.UNIAXIAL_STRESS:
+                self.D_0 = np.array([[self.E0]])
+                self.I2[0] = 1.0
+            case _:
+                msg = "Constraint not implemented"
+                raise NotImplementedError(msg)
 
     def evaluate(
         self,
@@ -59,12 +101,14 @@ class SpringKelvinModel(IncrSmallStrainModel):
         )
         # n_gauss = grad_del_u.size // (self.geometric_dim**2)
         mandel_view = mandel_stress.reshape(-1, self.stress_strain_dim)
-        print('mandel',mandel_view)
+        tangent_view = tangent.reshape(-1, self.stress_strain_dim ** 2)
         strain_increment = strain_from_grad_u(grad_del_u, self.constraint).reshape(-1, self.stress_strain_dim)
         print('strain inc',strain_increment)
         strain_visco_n = history['strain_visco'].reshape(-1, self.stress_strain_dim)
-        print('strain visco', strain_visco_n)
+        print('strain visco', strain_visco_n, history['strain_visco'])
         print(strain_visco_n[0], strain_visco_n[1])
+        strain_n = history['strain'].reshape(-1, self.stress_strain_dim)
+        print('strain', strain_n)
 
         # loop over gauss points
         print('del t', del_t)
@@ -75,23 +119,40 @@ class SpringKelvinModel(IncrSmallStrainModel):
 
             if del_t == 0:
                 # linear step visko strain is zero
-                dstress = self.E0 * eps
-                D = np.array([[self.E0]])
-            else:
-                # compute visko strain 1D case only !!! SAME as 3D with nu=0???
-                factor = (1 / del_t + 1 / self.tau + self.E0 / (self.tau * self.E1))
-                deps_visko = (mandel_view[n] / (self.tau * self.E1) + self.E0 / (self.tau * self.E1) * eps -
-                              strain_visco_n[n] / self.tau) / factor
-                dstress = self.E0 * (eps - deps_visko)
-                D = np.array([[self.E0]])* (1 - self.E0**2/(self.E1*self.tau *factor))
+                dstress = self.D_0 @ eps
+                D = self.D_0
 
-                print('D', D)
+            else:
+                factor = (1 / del_t + 1 / self.tau + self.mu0 / (self.tau * self.mu1))
+                print(factor)
+                deps_visko = 1/factor * (
+                              1 / (self.tau * 2 * self.mu1) * mandel_view[n]
+                              - 1 / self.tau * strain_visco_n[n]
+                              + self.mu0 / (self.tau * self.mu1) * eps
+                              + self.lam0 / (self.tau * 2 * self.mu1) *  np.sum(eps[:3]) * self.I2
+                              )
+
+                dstress = self.D_0 @ eps - 2*self.mu0 * deps_visko
+                D = (1 - self.mu0/(self.tau*self.mu1*factor)) * self.D_0
+
+
+                # # compute visko strain 1D case only !!! SAME as 3D with nu=0???
+                # factor = (1 / del_t + 1 / self.tau + self.E0 / (self.tau * self.E1))
+                # deps_visko = (mandel_view[n] / (self.tau * self.E1) + self.E0 / (self.tau * self.E1) * eps -
+                #               strain_visco_n[n] / self.tau) / factor
+
+                # dstress = self.E0 * (eps - deps_visko)
+                # D = np.array([[self.E0]])* (1 - self.E0**2/(self.E1*self.tau *factor))
+                #
+                # print('D', D)
 
                 # update values
-                history['strain_visco'][n] += deps_visko
+                strain_visco_n[n] += deps_visko
+
             mandel_view[n] += dstress
-            history['strain'][n] += eps
-            tangent[n] = D.flatten()
+            strain_n[n] += eps
+            tangent_view[n] = D.flatten()
+
 
 
     @property
--- examples/visco_elasticity/test_viscoelasticity.py
+++ examples/visco_elasticity/test_viscoelasticity.py
@@ -11,7 +11,7 @@ from mpi4py import MPI
 from fenics_constitutive import Constraint, IncrSmallStrainProblem
 
 youngs_modulus = 42.0
-poissons_ratio = 0.3
+poissons_ratio = 0.2
 visco_modulus = 10.0
 relaxation_time = 10.0
 
@@ -49,13 +49,19 @@ def test_relaxation_uniaxial_stress():
     time = [0]
     disp = []
     stress = []
+    strain = []
+    viscostrain = []
 
     # elastic first step
     problem._time = 0
     solver.solve(u)
     problem.update()
+
+    # store values last element/point
     disp.append(u.x.array[-1])
     stress.append(problem.stress_1.x.array[-1])
+    strain.append(problem._history_1[0]['strain'].x.array[-1])
+    viscostrain.append(problem._history_1[0]['strain_visco'].x.array[-1])
 
     # set time step and solve until total time
     dt = 2
@@ -65,21 +71,142 @@ def test_relaxation_uniaxial_stress():
         time.append(time[-1]+dt)
         niter, converged = solver.solve(u)
         problem.update()
-        print(f"time {time} Converged: {converged} in {niter} iterations.")
+        print(f"time {time[-1]} Converged: {converged} in {niter} iterations.")
 
         # print(problem.stress_1.x.array)  # mandel stress at time t
         # print(u.x.array)
         disp.append(u.x.array[-1])
         stress.append(problem.stress_1.x.array[-1])
+        strain.append(problem._history_1[0]['strain'].x.array[-1])
+        viscostrain.append(problem._history_1[0]['strain_visco'].x.array[-1])
 
+    # print(disp, stress)
+    # print(strain, viscostrain)
     #analytic solution for 1D Kelvin model
     stress_0_ana = youngs_modulus * displacement.value/1.
     stress_final_ana = youngs_modulus * visco_modulus / (youngs_modulus + visco_modulus) * displacement.value/1.
 
     assert abs(stress[0] - stress_0_ana) < 1e-8
     assert abs(stress[-1] - stress_final_ana) < 1e-8
+    assert abs(strain[0] - displacement.value/1) < 1e-8
 
+    # sanity checks
+    assert np.sum(np.diff(strain)) < 1e-8
+    assert abs(viscostrain[0] - 0) < 1e-8
+    assert viscostrain[-1] > 0
+
+@pytest.mark.parametrize(constraint = ['plane_stress', 'plane_strain', 'full'])
+@pytest.mark.parametrize(dim = [2,3])
+def test_relaxation(constraint: str, dim: int):
+    if dim == 2:
+        mesh = df.mesh.create_unit_square(MPI.COMM_WORLD, 2, 2)
+        bc_vector = np.array([0.0, 0.0])
+    elif dim == 3:
+        mesh = df.mesh.create_unit_cube(MPI.COMM_WORLD, 2, 2, 2)
+        bc_vector = np.array([0.0, 0.0, 0.0])
+    else:
+        raise ValueError(f"Dimension {dim} not supported")
+
+    V = df.fem.VectorFunctionSpace(mesh, ("CG", 1))
+    u = df.fem.Function(V)
+    if constraint == 'plane_stress':
+        law = SpringKelvinModel(
+            parameters={"E0": youngs_modulus, "E1": visco_modulus, "tau": relaxation_time, "nu": poissons_ratio},
+            constraint=Constraint.UNIAXIAL_STRESS,
+        )
+    elif constraint == 'plane_strain':
+        law = SpringKelvinModel(
+            parameters={"E0": youngs_modulus, "E1": visco_modulus, "tau": relaxation_time, "nu": poissons_ratio},
+            constraint=Constraint.PLANE_STRAIN,
+        )
+    elif constraint == 'full':
+        law = SpringKelvinModel(
+            parameters={"E0": youngs_modulus, "E1": visco_modulus, "tau": relaxation_time, "nu": poissons_ratio},
+            constraint=Constraint.FULL,
+        )
+    else:
+        raise ValueError(f"Constraint {constraint} not supported")
+
+    def left_boundary(x):
+        return np.isclose(x[0], 0.0)
+
+    def right_boundary(x):
+        return np.isclose(x[0], 1.0)
+
+    displacement = 0.01
+    dofs_left = df.fem.locate_dofs_geometrical(V, left_boundary)
+    dofs_right = df.fem.locate_dofs_geometrical(V, right_boundary)
+    bc_left = df.fem.dirichletbc(bc_vector, dofs_left, V)
+    # displacement in x direction
+    disp_vector = bc_vector.copy()
+    disp_vector[0] = displacement
+    bc_right = df.fem.dirichletbc(disp_vector, dofs_right, V)
+
+    problem = IncrSmallStrainProblem(
+        law,
+        u,
+        [bc_left, bc_right],
+        1,
+    )
+
+    solver = NewtonSolver(MPI.COMM_WORLD, problem)
+
+    time = [0]
+    disp = []
+    stress = []
+    strain = []
+    viscostrain = []
+
+    # elastic first step
+    problem._time = 0
+    solver.solve(u)
+    problem.update()
+    print(u.x.array)
+    print(problem.stress_1.x.array)
+
+
+    strain.append(problem._history_1[0]['strain'].x.array[0])
+    viscostrain.append(problem._history_1[0]['strain_visco'].x.array[0])
+    disp.append(u.x.array.max())
+    stress.append(problem.stress_1.x.array.max())
+
+
+    # set time step and solve until total time
+    dt = 2
+    problem._time = dt
+    total_time = 10 * relaxation_time
+    while time[-1] < total_time:
+        time.append(time[-1] + dt)
+        niter, converged = solver.solve(u)
+        problem.update()
+        print(f"time {time} Converged: {converged} in {niter} iterations.")
+
+        # print(problem.stress_1.x.array)  # mandel stress at time t
+        # print(u.x.array)
+        disp.append(u.x.array.max())
+        stress.append(problem.stress_1.x.array.max())
+        strain.append(problem._history_1[0]['strain'].x.array[0])
+        viscostrain.append(problem._history_1[0]['strain_visco'].x.array[0])
+
+    print(disp, stress)
+    print(strain)
+    # analytic solution for 1D Kelvin model
+    stress_0_ana = youngs_modulus * displacement/1.
+    stress_final_ana = youngs_modulus * visco_modulus / (youngs_modulus + visco_modulus) * displacement/1.
+    print('ana', stress_0_ana, stress_final_ana)
+
+    # # sanity check if out of plane stress is NOT zero
+    # assert (
+    #     np.linalg.norm(
+    #         problem.stress_0.x.array.reshape(-1, law.constraint.stress_strain_dim())[
+    #             :, 2
+    #         ]
+    #     )
+    #     > 1e-2
+    # )
 
 
 if __name__ == "__main__":
-    test_relaxation_uniaxial_stress()
+    # test_relaxation_uniaxial_stress()
+
+    test_relaxation('full',3)
