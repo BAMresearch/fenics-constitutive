3b684c9969bfc791d7faaac5b01ca05b9bc55f20
srosenbu <sjard-mathis.rosenbusch@bam.de>
2025-11-17 16:35:42 +0100

    WIP

--- src/fenics_constitutive/models/interfaces.py
+++ src/fenics_constitutive/models/interfaces.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
 from enum import Enum
 
 import numpy as np
@@ -86,6 +87,84 @@ class IncrSmallStrainModel(ABC):
         grad_del_u: np.ndarray,
         stress: np.ndarray,
         tangent: np.ndarray,
+        history: dict[str, np.ndarray] | None,
+    ) -> None:
+        r"""
+        Evaluate the constitutive model and overwrite the stress, tangent and history.
+
+        Args:
+            t: The current global time $t_n$.
+            del_t: The time increment $\Delta t$. The time at the end of the increment is $t_{n+1}=t_n+\Delta t$.
+            grad_del_u: The gradient of the increment of the displacement field $\nabla\delta$ with $\delta=u_{n+1}-u_n$.
+            stress: The current stress in Mandel notation.
+            tangent: The tangent compatible with Mandel notation.
+            history: The history variable(s).
+        """
+
+    @property
+    @abstractmethod
+    def constraint(self) -> StressStrainConstraint:
+        """
+        The constraint for the stresses or the strains.
+
+        Returns:
+            The constraint.
+        """
+
+    @property
+    def stress_strain_dim(self) -> int:
+        """
+        The stress-strain dimension that the model is implemented for.
+
+        Returns:
+            The stress-strain dimension.
+        """
+        return self.constraint.stress_strain_dim
+
+    @property
+    def geometric_dim(self) -> int:
+        """
+        The geometric dimension that the model is implemented for.
+
+        Returns:
+            The geometric dimension.
+        """
+        return self.constraint.geometric_dim
+
+    @property
+    @abstractmethod
+    def history_dim(self) -> dict[str, int | tuple[int, int]] | None:
+        """
+        The dimensions of history variable(s). This is needed to tell the solver which quadrature
+        spaces or arrays to build. If it is not none, a dictionary is returned with the name of the
+        history variable as key and the dimension of the history variable as value.
+
+        Returns:
+            The dimension of the history variable(s).
+        """
+
+@dataclass
+class NonlocalTangents:
+    dsigma_deps: np.ndarray
+    dsigma_dnonlocal: np.ndarray
+    dlocal_deps: np.ndarray
+    dlocal_dnonlocal: np.ndarray
+
+class IncrSmallStrainGradientModel(ABC):
+    """
+    Interface for incremental small strain models with gradient plasticity and/or damage formulation.
+    """
+    
+    @abstractmethod
+    def evaluate(
+        self,
+        t: float,
+        del_t: float,
+        grad_del_u: np.ndarray,
+        nonlocal_quantity: np.ndarray,
+        stress: np.ndarray,
+        local_quantity: np.ndarray,
+        tangents: NonlocalTangents | None,
         history: dict[str, np.ndarray] | None,
     ) -> None:
         r"""
