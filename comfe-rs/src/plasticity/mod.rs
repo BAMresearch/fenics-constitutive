3b7c7185441d5c277bfd54767938582e751c46f6
srosenbu <sjard-mathis.rosenbusch@bam.de>
2025-12-08 14:25:18 +0100

    WIP

--- comfe-rs/src/plasticity/mod.rs
+++ comfe-rs/src/plasticity/mod.rs
@@ -1,8 +1,10 @@
 pub mod general;
+pub mod general_gradient_enhanced;
 pub mod drucker_prager_classic;
 pub mod drucker_prager_hyperbolic;
 
 // Re-export everything from both modules for backward compatibility
 pub use general::*;
+pub use general_gradient_enhanced::*;
 pub use drucker_prager_classic::*;
 pub use drucker_prager_hyperbolic::*;
