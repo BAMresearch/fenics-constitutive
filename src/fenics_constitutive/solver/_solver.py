15de1e30128740bce511f4fac6fac23a0a571d0a
srosenbu <sjard-mathis.rosenbusch@bam.de>
2026-02-20 09:18:39 +0100

    Order of parameters

--- src/fenics_constitutive/solver/_solver.py
+++ src/fenics_constitutive/solver/_solver.py
@@ -57,8 +57,8 @@ class IncrSmallStrainProblem(NonlinearProblem):
         u: df.fem.Function,
         bcs: list[df.fem.DirichletBC],
         q_degree: int,
-        external_forces: list[ufl.Form] | None = None,
         del_t: float = 1.0,
+        external_forces: list[ufl.Form] | None = None,
         form_compiler_options: dict | None = None,
         jit_options: dict | None = None,
     ) -> None:
