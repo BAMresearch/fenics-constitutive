59903f0c9499306a6a36de0518be65cf6362d7de
srosenbu <sjard-mathis.rosenbusch@bam.de>
2026-02-23 19:51:19 +0100

    Test the tangent for two different von mises models

--- tests/models/test_elastoplastic_tangent.py
+++ tests/models/test_elastoplastic_tangent.py
@@ -27,7 +27,7 @@ def create_history(law:IncrSmallStrainModel)-> dict[str,np.ndarray]:
     return output
 
 
-def test_tangent_3d():
+def test_tangent_3d_uniaxial_strain():
     
     sigma_analytical = np.zeros(6)
     sigma_numerical = np.zeros(6)
@@ -64,10 +64,97 @@ def test_tangent_3d():
         law_analytical.evaluate(0.0,0.0, grad_del_u.flatten(),sigma_analytical,tangent_analytical, history_analytical)
         law_numerical.evaluate(0.0,0.0, grad_del_u.flatten(),sigma_numerical,tangent_numerical, history_numerical)
         # Compare stress
-        assert np.allclose(sigma_analytical, sigma_numerical, atol=1e-6, rtol=1e-10), f"Stress mismatch at iteration {i}"
+        assert np.allclose(sigma_analytical, sigma_numerical, atol=1e-12, rtol=1e-14), f"Stress mismatch at iteration {i}"
         # Compare tangent
-        assert np.allclose(tangent_analytical, tangent_numerical, atol=1e-6, rtol=1e-10), f"Tangent mismatch at iteration {i}"
+        assert np.allclose(tangent_analytical, tangent_numerical, atol=1e-12, rtol=1e-14), f"Tangent mismatch at iteration {i}"
 
     assert history_analytical["history"][0] > 0, "plastic strain not reached in test"
 
     
+def test_tangent_3d_plane_strain():
+    
+    sigma_analytical = np.zeros(6)
+    sigma_numerical = np.zeros(6)
+
+    tangent_analytical = np.zeros(36)
+    tangent_numerical = np.zeros(36)
+
+    #parameters for steel
+    matparam = {
+        "kappa": 166.67,  # Bulk modulus
+        "mu": 80.77,    # Shear modulus
+        "y_0": 0.2,      # Initial yield stress
+        "h": 10.0,       # Hardening modulus
+    }
+    matparam_arr = {
+        "mu": np.array([matparam["mu"]]),
+        "kappa": np.array([matparam["kappa"]]),
+        "y_0":np.array([matparam["y_0"]]),
+        "h":np.array([matparam["h"]]),
+    }
+
+    law_analytical = IsotropicMises3D(matparam_arr)
+    law_numerical = MisesPlasticityLinearHardening3D(matparam_arr)
+
+    # Create history variables
+    history_analytical = create_history(law_analytical)
+    history_numerical = create_history(law_numerical)
+
+    # del_grad_u for uniaxial strain
+    grad_del_u = np.zeros((3, 3))
+    grad_del_u[0, 0] = 0.001
+    grad_del_u[1, 1] = 0.001
+
+    for i in range(10):
+        law_analytical.evaluate(0.0,0.0, grad_del_u.flatten(),sigma_analytical,tangent_analytical, history_analytical)
+        law_numerical.evaluate(0.0,0.0, grad_del_u.flatten(),sigma_numerical,tangent_numerical, history_numerical)
+        # Compare stress
+        assert np.allclose(sigma_analytical, sigma_numerical, atol=1e-12, rtol=1e-14), f"Stress mismatch at iteration {i}"
+        # Compare tangent
+        assert np.allclose(tangent_analytical, tangent_numerical, atol=1e-12, rtol=1e-14), f"Tangent mismatch at iteration {i}"
+
+    assert history_analytical["history"][0] > 0, "plastic strain not reached in test"
+
+
+def test_tangent_3d_random():
+    
+    sigma_analytical = np.zeros(6)
+    sigma_numerical = np.zeros(6)
+
+    tangent_analytical = np.zeros(36)
+    tangent_numerical = np.zeros(36)
+
+    #parameters for steel
+    matparam = {
+        "kappa": 166.67,  # Bulk modulus
+        "mu": 80.77,    # Shear modulus
+        "y_0": 0.2,      # Initial yield stress
+        "h": 10.0,       # Hardening modulus
+    }
+    matparam_arr = {
+        "mu": np.array([matparam["mu"]]),
+        "kappa": np.array([matparam["kappa"]]),
+        "y_0":np.array([matparam["y_0"]]),
+        "h":np.array([matparam["h"]]),
+    }
+
+    law_analytical = IsotropicMises3D(matparam_arr)
+    law_numerical = MisesPlasticityLinearHardening3D(matparam_arr)
+
+    # Create history variables
+    history_analytical = create_history(law_analytical)
+    history_numerical = create_history(law_numerical)
+
+    # del_grad_u for uniaxial strain
+    grad_del_u = np.random.random((3,3))
+    grad_del_u = 0.01*grad_del_u/np.linalg.norm(grad_del_u)
+
+    for i in range(10):
+        law_analytical.evaluate(0.0,0.0, grad_del_u.flatten(),sigma_analytical,tangent_analytical, history_analytical)
+        law_numerical.evaluate(0.0,0.0, grad_del_u.flatten(),sigma_numerical,tangent_numerical, history_numerical)
+        # Compare stress
+        assert np.allclose(sigma_analytical, sigma_numerical, atol=1e-12, rtol=1e-14), f"Stress mismatch at iteration {i}"
+        # Compare tangent
+        assert np.allclose(tangent_analytical, tangent_numerical, atol=1e-12, rtol=1e-14), f"Tangent mismatch at iteration {i}"
+
+    assert history_analytical["history"][0] > 0, "plastic strain not reached in test"
